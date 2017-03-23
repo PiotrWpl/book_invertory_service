442be08a77ea814be2ae445703368aa3458e4332
PiotrWpl <piotrw14@gmail.com>
2017-03-23 11:14:25 +0100

    express.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /*jslint node: true */
 'use strict';
 
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.end('hello world');
-});
-server.listen(3000, function () {
-  console.log("When this callback is invoked our server is listening on port: " + 3000);
-});
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
